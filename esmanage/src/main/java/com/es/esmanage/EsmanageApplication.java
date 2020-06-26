b1be593ba522efb6b8b099949960ab729f682b80
tianwamg <987897856@qq.com>
2020-06-26 23:31:39 +0800

    elasticsearch

---
+++ esmanage/src/main/java/com/es/esmanage/EsmanageApplication.java
@@ -0,0 +1,13 @@
+package com.es.esmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsmanageApplication.class, args);
+    }
+
+}
