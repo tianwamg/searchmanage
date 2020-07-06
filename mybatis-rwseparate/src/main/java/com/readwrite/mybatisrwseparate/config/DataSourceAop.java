012cc4b562d4d149a706b265635fc8902276424d
wangpp <987897856@qq.com>
2020-07-06 18:55:59 +0800

    mybatis 读写分离

--- mybatis-rwseparate/src/main/java/com/readwrite/mybatisrwseparate/config/DataSourceAop.java
+++ mybatis-rwseparate/src/main/java/com/readwrite/mybatisrwseparate/config/DataSourceAop.java
@@ -1,8 +1,12 @@
 package com.readwrite.mybatisrwseparate.config;
 
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class DataSourceAop {
 
     @Pointcut("!@annotation(com.cjs.example.annotation.Master) " +
